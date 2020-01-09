a50db17c0adeb62d0da13e974b2c26fab256065c
hannah <hannah.stebnicki@gmail.com>
2020-01-09 12:56:17 -0800

    Updates

---
+++ precache-manifest.65fd8ca971096bcffd3b0c5926ce812a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "782e4c02329b9f38de92c818e85650c6",
+    "url": "/hannahstebnicki/robofriends/index.html"
+  },
+  {
+    "revision": "84342310c18891d40c97",
+    "url": "/hannahstebnicki/robofriends/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "3309c839d8b510f621db",
+    "url": "/hannahstebnicki/robofriends/static/css/main.bb28465e.chunk.css"
+  },
+  {
+    "revision": "84342310c18891d40c97",
+    "url": "/hannahstebnicki/robofriends/static/js/2.167b5057.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/hannahstebnicki/robofriends/static/js/2.167b5057.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3309c839d8b510f621db",
+    "url": "/hannahstebnicki/robofriends/static/js/main.b65be977.chunk.js"
+  },
+  {
+    "revision": "09fe7e8c82e542e2ef98",
+    "url": "/hannahstebnicki/robofriends/static/js/runtime-main.f5ca607f.js"
+  }
+]);
