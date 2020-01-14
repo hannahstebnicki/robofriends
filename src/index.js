61f84f21b4b3a2c5d63fd19a806f244ca9d1976b
hannah <hannah.stebnicki@gmail.com>
2020-01-14 14:24:16 -0800

    updated state management system

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import thunkMiddleware from 'redux-thunk';
+import { Provider, connect } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import App from './containers/App';
+import { searchRobots, requestRobots } from './reducers';
+import { createLogger } from 'redux-logger';
 
+const logger = createLogger();
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
-    <App />, document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root')
 );
 
 
